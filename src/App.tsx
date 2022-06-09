1505c63a276e404c9c5aa464917f35e40d9ae2b1
Irakli <i.botchori@gmail.com>
2022-06-09 20:29:14 +0400

    wip: add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Login from "./pages/Login/Login";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <div>
+      <Login />
+    </div>
   );
 }
 
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div className="w-full h-full flex">
+      <div className="w-full md:w-3/5  h-screen text-center justify-center ">
+        Content
+      </div>
+      <div className="w-2/5 hidden md:block h-screen">
+        <div className=" w-full h-full bg-no-repeat bg-cover bg-center bg-[url('./assets/img/cover_image.png')]"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
