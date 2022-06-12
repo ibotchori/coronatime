0658d268fe97bb8d33552f8dea88a1ce3a88f130
Irakli <i.botchori@gmail.com>
2022-06-12 15:41:32 +0400

    feat: add SignUp page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import SignUp from "pages/SignUp/SignUp";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
       <Login />
+      <SignUp />
     </div>
   );
 }
---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from "react";
+import {
+  Button,
+  CoverImage,
+  Input,
+  MainLogo,
+  Title,
+  Checkbox,
+  HaveAccount,
+} from "components/";
+
+type Props = {};
+
+const SignUp = (props: Props) => {
+  return (
+    <div className="w-full h-full flex">
+      <div className="w-full md:w-[55%] flex justify-center md:justify-start  h-screen  ">
+        <div className=" md:w-fit lg:w-full pl-5 md:pl-16 lg:pl-28 pt-7 overflow-hidden pr-5 ">
+          {/* Logo */}
+          <MainLogo />
+          {/* Title */}
+          <Title
+            mainText="Welcome to Coronatime"
+            paragraph="Please enter required info to sign up"
+          />
+          <form className=" max-w-[400px] ">
+            {/* Input */}
+            <Input
+              title="Username"
+              type="text"
+              id="username"
+              placeholder="Enter unique username or email"
+            />
+            <Input
+              title="Email"
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+            />
+            <Input
+              title="Password"
+              type="text"
+              id="password"
+              placeholder="Fill in password"
+            />
+            <Input
+              title="Repeat password"
+              type="text"
+              id="password"
+              placeholder="Repeat password"
+            />
+            {/* Checkbox */}
+            <Checkbox label="Remember this device" anchor="" />
+            {/* Button */}
+            <Button title="Log In" />
+            {/* Have Account */}
+            <HaveAccount title="Already have an account?" anchor="Log in" />
+          </form>
+        </div>
+      </div>
+      <CoverImage />
+    </div>
+  );
+};
+
+export default SignUp;
