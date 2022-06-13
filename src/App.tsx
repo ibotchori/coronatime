1ddf9110e8eec60ff68a5bb9cc1e02deea81ad8c
Irakli <i.botchori@gmail.com>
2022-06-13 18:41:20 +0400

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import "./i18n";
 import ResetPassword from "pages/ResetPassword/ResetPassword";
 import NewPassword from "pages/NewPassword/NewPassword";
 import Confirmation from "pages/Confirmation/Confirmation";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Login />
-      <SignUp />
-      <ResetPassword />
-      <NewPassword />
-      <Confirmation />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/new-password" element={<NewPassword />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
