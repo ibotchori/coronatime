a934521d6dc39ae1aff8bbaa2ef059f168882c75
Irakli <i.botchori@gmail.com>
2022-06-14 21:28:24 +0400

    feat: add toggle buttons on Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,6 +6,7 @@ type Props = {};
 const Dashboard = (props: Props) => {
   return (
     <div className="w-full min-h-screen py-4">
+      {/* Header */}
       <div className="flex justify-between border-b-2 border-gray-200 px-4 sm:px-10 md:px-20 py-3">
         <div className="pb-3 ">
           <div className="sm:hidden">
@@ -27,7 +28,22 @@ const Dashboard = (props: Props) => {
           </div>
         </div>
       </div>
-      <div></div>
+      {/* Toggle Buttons */}
+      <div className=" px-4 sm:px-10 md:px-20">
+        <p className="font-bold text-2xl pt-12 pb-10">Worldwide Statistics</p>
+        <div className="border-b-2 pb-3 text-base flex ">
+          <div className="w-[100px] sm:w-[130px]">
+            <span className="hover:font-bold cursor-pointer hover:border-b-4 hover:border-black pb-3">
+              Worldwide
+            </span>
+          </div>
+          <div>
+            <span className="hover:font-bold cursor-pointer hover:border-b-4 hover:border-black pb-3">
+              By Country
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
