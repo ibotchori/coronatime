bd442732c9e453e31603ae2e481f8de9edf51bf6
Irakli <i.botchori@gmail.com>
2022-06-17 17:34:29 +0400

    feat: add icon in input field conditionally

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -29,19 +29,37 @@ const Input: React.FC<Props> = ({
       >
         {title}
       </label>
-      <input
-        type={type}
-        id={id}
-        className={`bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-300 focus:outline-none focus:border-blue-300 focus:ring-1 focus:ring-blue-300  block w-full p-4 border  ${
-          errorMessage
-            ? "border-red-400"
-            : dirtyFields && !errorMessage
-            ? "border-green-400"
-            : " "
-        } `}
-        placeholder={placeholder}
-        {...register(name)}
-      />
+      <div className="relative">
+        {dirtyFields && !errorMessage && (
+          <svg
+            className="absolute right-1 mt-4 mr-2"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM11.003 16L18.073 8.929L16.659 7.515L11.003 13.172L8.174 10.343L6.76 11.757L11.003 16Z"
+              fill="#249E2C"
+            />
+          </svg>
+        )}
+        <input
+          type={type}
+          id={id}
+          className={`bg-gray-50 text-gray-900 text-sm rounded-lg border-gray-300 focus:outline-none focus:border-blue-300 focus:ring-1 focus:ring-blue-300  block w-full p-4 border  ${
+            errorMessage
+              ? "border-red-400"
+              : dirtyFields && !errorMessage
+              ? "border-green-400"
+              : " "
+          } `}
+          placeholder={placeholder}
+          {...register(name)}
+        />
+      </div>
+
       {errorMessage && (
         <div className="h-[0.1px] flex text-[10px] sm:text-xs ">
           <svg
