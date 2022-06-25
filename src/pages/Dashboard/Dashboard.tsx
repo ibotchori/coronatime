24dd003f807e7c8f74f575ff65d7c5e610ec3e3d
Irakli <i.botchori@gmail.com>
2022-06-25 22:19:17 +0400

    fix: sort by location on Dashboard table

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -64,9 +64,9 @@ const Dashboard = (props: Props) => {
 
   useEffect(() => {
     if (locationButtonSort) {
-      filteredData.reverse();
+      fetchedData?.sort((a: any, b: any) => (a.name.en > b.name.en ? 1 : -1));
     } else {
-      filteredData.reverse();
+      fetchedData?.sort((a: any, b: any) => (a.name.en > b.name.en ? -1 : 1));
     }
   }, [filteredData, locationButtonSort, i18n.language]);
 
